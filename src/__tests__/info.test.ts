8ca905a8a1a7983f165584dd1bf565e3f10685b0
Filip Stenbacka <filip.stenbacka@gmail.com>
2022-07-06 03:01:25 +0300

    Added tests that tests the simple test database.

--- src/__tests__/info.test.ts
+++ src/__tests__/info.test.ts
@@ -1,31 +1,77 @@
 import { parseInfo } from "../util";
 
-const str = `
- filename = "random.rrd"
- rrd_version = "0001"
- step = 300
- last_update = 955892996
- header_size = 2872
- ds[a].type = "GAUGE"
- ds[a].minimal_heartbeat = 600
- ds[a].min = NaN
- ds[a].max = NaN
- ds[a].last_ds = "UNKN"
- ds[a].value = 2.1824421548e+04
- ds[a].unknown_sec = 0
- ds[b].type = "GAUGE"
- ds[b].minimal_heartbeat = 600
- ds[b].min = NaN
- ds[b].max = NaN
- ds[b].last_ds = "UNKN"
- ds[b].value = 3.9620838224e+03
- ds[b].unknown_sec = 0
- rra[0].cf = "AVERAGE"
- rra[0].pdp_per_row = 1
- rra[0].cdp_prep[0].value = nan
- rra[0].cdp_prep[0].unknown_datapoints = 0
- rra[0].cdp_prep[1].value = nan
- rra[0].cdp_prep[1].unknown_datapoints = 0
-`;
-
-console.log(parseInfo(str));
+test("parse info string", () => {
+  expect(
+    parseInfo(`
+        filename = "random.rrd"
+        rrd_version = "0001"
+        step = 300
+        last_update = 955892996
+        header_size = 2872
+        ds[a].type = "GAUGE"
+        ds[a].minimal_heartbeat = 600
+        ds[a].min = NaN
+        ds[a].max = NaN
+        ds[a].last_ds = "UNKN"
+        ds[a].value = 2.1824421548e+04
+        ds[a].unknown_sec = 0
+        ds[b].type = "GAUGE"
+        ds[b].minimal_heartbeat = 600
+        ds[b].min = NaN
+        ds[b].max = NaN
+        ds[b].last_ds = "UNKN"
+        ds[b].value = 3.9620838224e+03
+        ds[b].unknown_sec = 0
+        rra[0].cf = "AVERAGE"
+        rra[0].pdp_per_row = 1
+        rra[0].cdp_prep[0].value = nan
+        rra[0].cdp_prep[0].unknown_datapoints = 0
+        rra[0].cdp_prep[1].value = nan
+        rra[0].cdp_prep[1].unknown_datapoints = 0
+    `)
+  ).toEqual({
+    filename: "random.rrd",
+    rrd_version: "0001",
+    step: 300,
+    last_update: 955892996,
+    header_size: 2872,
+    ds: [
+      {
+        name: "a",
+        type: "GAUGE",
+        minimal_heartbeat: 600,
+        min: NaN,
+        max: NaN,
+        last_ds: "UNKN",
+        value: 2.1824421548e4,
+        unknown_sec: 0,
+      },
+      {
+        name: "b",
+        type: "GAUGE",
+        minimal_heartbeat: 600,
+        min: NaN,
+        max: NaN,
+        last_ds: "UNKN",
+        value: 3.9620838224e3,
+        unknown_sec: 0,
+      },
+    ],
+    rra: [
+      {
+        cf: "AVERAGE",
+        pdp_per_row: 1,
+        cdp_prep: [
+          {
+            value: NaN,
+            unknown_datapoints: 0,
+          },
+          {
+            value: NaN,
+            unknown_datapoints: 0,
+          },
+        ],
+      },
+    ],
+  });
+});
---
+++ src/__tests__/rrdtool.test.ts
@@ -0,0 +1,105 @@
+import rrdtool, { RrdtoolInfo, RrdtoolDatabase } from "..";
+import path from "path";
+
+const p = path.join(__dirname, "_simple.rrd");
+const start = 1405942000;
+
+describe("simple test database", () => {
+  let db: RrdtoolDatabase<{ test: number }>;
+  beforeEach(() => {
+    db = rrdtool.open(p);
+  });
+
+  test("open", async () => {
+    expect(db).toBeTruthy();
+  });
+
+  test("info", async () => {
+    const info: RrdtoolInfo = {
+      filename: p,
+      rrd_version: "0003",
+      step: 1,
+      last_update: 1405942010,
+      header_size: 792,
+      ds: [
+        {
+          name: "test",
+          index: 0,
+          type: "GAUGE",
+          minimal_heartbeat: 1,
+          min: 0,
+          max: 100,
+          last_ds: "75",
+          value: 0,
+          unknown_sec: 0,
+        },
+      ],
+      rra: [
+        {
+          cf: "AVERAGE",
+          rows: 20,
+          cur_row: 11,
+          pdp_per_row: 1,
+          xff: 0,
+          cdp_prep: [
+            {
+              value: NaN,
+              unknown_datapoints: 0,
+            },
+          ],
+        },
+        {
+          cf: "MAX",
+          rows: 1,
+          cur_row: 0,
+          pdp_per_row: 10,
+          xff: 0,
+          cdp_prep: [
+            {
+              value: NaN,
+              unknown_datapoints: 0,
+            },
+          ],
+        },
+      ],
+    };
+
+    expect(await db.info()).toEqual(info);
+  });
+
+  test("dump", async () => {
+    const db = rrdtool.open(p);
+    const dump = await db.dump();
+    expect(typeof dump).toBe("string");
+    expect(dump).toContain("Round Robin Database Dump");
+  });
+
+  test("fetch no arguments", async () => {
+    const data = await db.fetch("AVERAGE");
+    expect(data).toHaveLength(60*60*24 + 1);
+  });
+
+  test("fetch start and end", async () => {
+    const end = start + 10;
+    let data = await db.fetch("AVERAGE", { start, end });
+    expect(data).toHaveLength(11);
+    expect(data[0].timestamp).toBe(start);
+    expect(data[10].timestamp).toBe(end);
+    expect(data.map(d => d.values["test"])).toEqual([35, 75, 50, 85, 65, 0, 20, 95, 25, 0, 75]);
+
+    data = await db.fetch("AVERAGE", { start: start + 5, end: end + 5 });
+    expect(data).toHaveLength(11);
+    expect(data[0].timestamp).toBe(start + 5);
+    expect(data[10].timestamp).toBe(end + 5);
+    expect(data.map(d => d.values["test"])).toEqual([0, 20, 95, 25, 0, 75, undefined, undefined, undefined, undefined, undefined]);
+  });
+
+  test("update wrong values", async () => {
+    expect.assertions(1);
+    try {
+      await db.update({ invalid: 123 } as any);
+    } catch (e) {
+      expect(e).toBeTruthy();
+    }
+  });
+});
